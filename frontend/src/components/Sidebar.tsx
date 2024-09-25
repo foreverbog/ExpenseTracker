a4da28f55f11dde1ce37adbd669b7faaca030477
foreverbog <133509015+foreverbog@users.noreply.github.com>
2024-09-25 18:10:24 +0200

    BugFix
    Landing Page button is not more clickable and redirecting when sidebar is open

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -45,86 +45,91 @@ const Sidebar: React.FC<SidebarProps> = ({ isSideBarOpen, handleSideBar }) => {
   };
 
   return (
-    <motion.div
-      initial={{ width: 0 }}
-      animate={{ width: "50%" }}
-      exit={{ width: 0, transition: { duration: 0.3 } }}
-      transition={{
-        type: "spring",
-        stiffness: 100,
-      }}
-      ref={sideBarRef}
-      className="bg-base-200 font-base text-base-text absolute top-0 right-0 min-h-dvh z-50 w-1/2 shadow-[0_0_0_10000px_rgba(0,0,0,.60)] flex flex-col items-center justify-start overflow-hidden "
-    >
-      {isAuthenticated && (
-        <div className="flex flex-wrap gap-2 text-4xl mt-24">
-          <p>{user.firstName}</p>
-          <p>{user.lastName}</p>
+    <>
+      {isSideBarOpen && (
+        <div className="fixed inset-0 z-40 bg-black opacity-60"></div>
+      )}
+      <motion.div
+        initial={{ width: 0 }}
+        animate={{ width: "50%" }}
+        exit={{ width: 0, transition: { duration: 0.3 } }}
+        transition={{
+          type: "spring",
+          stiffness: 100,
+        }}
+        ref={sideBarRef}
+        className="bg-base-200 font-base text-base-text absolute top-0 right-0 min-h-dvh z-50 w-1/2  flex flex-col items-center justify-start overflow-hidden "
+      >
+        {isAuthenticated && (
+          <div className="flex flex-wrap gap-2 text-4xl mt-24">
+            <p>{user.firstName}</p>
+            <p>{user.lastName}</p>
+          </div>
+        )}
+        <div className="flex flex-col flex-1 gap-3 justify-center items-center ">
+          <NavLink
+            onClick={handleSideBar}
+            to="/home"
+            className={({ isActive }) =>
+              `flex items-center gap-2 text-lg xs:text-2xl  ${
+                isActive &&
+                "underline underline-offset-4 decoration-primary decoration-2"
+              }`
+            }
+          >
+            <FaHome />
+            <p>{t("nav.home")}</p>
+          </NavLink>
+          <NavLink
+            onClick={handleSideBar}
+            to="/about"
+            className={({ isActive }) =>
+              `flex items-center gap-2 text-lg xs:text-2xl  ${
+                isActive &&
+                "underline underline-offset-4 decoration-primary decoration-2"
+              }`
+            }
+          >
+            <FaInfo />
+            <p>{t("nav.about")}</p>
+          </NavLink>
+          <NavLink
+            onClick={handleSideBar}
+            to="/contact"
+            className={({ isActive }) =>
+              `flex items-center gap-2 text-lg xs:text-2xl  ${
+                isActive &&
+                "underline underline-offset-4 decoration-primary decoration-2"
+              }`
+            }
+          >
+            <FiMessageSquare />
+            <p>{t("nav.contact")}</p>
+          </NavLink>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              `flex items-center gap-2 text-lg xs:text-2xl  ${
+                isActive &&
+                "underline underline-offset-4 decoration-primary decoration-2"
+              }`
+            }
+          >
+            <IoMdSettings />
+            <p>{t("nav.settings")}</p>
+          </NavLink>
         </div>
-      )}
-      <div className="flex flex-col flex-1 gap-3 justify-center items-center ">
-        <NavLink
-          onClick={handleSideBar}
-          to="/home"
-          className={({ isActive }) =>
-            `flex items-center gap-2 text-lg xs:text-2xl  ${
-              isActive &&
-              "underline underline-offset-4 decoration-primary decoration-2"
-            }`
-          }
-        >
-          <FaHome />
-          <p>{t("nav.home")}</p>
-        </NavLink>
-        <NavLink
-          onClick={handleSideBar}
-          to="/about"
-          className={({ isActive }) =>
-            `flex items-center gap-2 text-lg xs:text-2xl  ${
-              isActive &&
-              "underline underline-offset-4 decoration-primary decoration-2"
-            }`
-          }
-        >
-          <FaInfo />
-          <p>{t("nav.about")}</p>
-        </NavLink>
-        <NavLink
-          onClick={handleSideBar}
-          to="/contact"
-          className={({ isActive }) =>
-            `flex items-center gap-2 text-lg xs:text-2xl  ${
-              isActive &&
-              "underline underline-offset-4 decoration-primary decoration-2"
-            }`
-          }
-        >
-          <FiMessageSquare />
-          <p>{t("nav.contact")}</p>
-        </NavLink>
-        <NavLink
-          to="/"
-          className={({ isActive }) =>
-            `flex items-center gap-2 text-lg xs:text-2xl  ${
-              isActive &&
-              "underline underline-offset-4 decoration-primary decoration-2"
-            }`
-          }
-        >
-          <IoMdSettings />
-          <p>{t("nav.settings")}</p>
-        </NavLink>
-      </div>
-      {isAuthenticated && (
-        <button
-          onClick={handleLogout}
-          className="bg-red-700 flex justify-center items-center gap-2 rounded-md w-3/4 sm:w-1/2   py-2 hover:bg-red-800 mb-24 overflow-hidden text-lg xs:text-lg "
-        >
-          <IoIosLogOut />
-          <p>{t("nav.logout")}</p>
-        </button>
-      )}
-    </motion.div>
+        {isAuthenticated && (
+          <button
+            onClick={handleLogout}
+            className="bg-red-700 flex justify-center items-center gap-2 rounded-md w-3/4 sm:w-1/2   py-2 hover:bg-red-800 mb-24 overflow-hidden text-lg xs:text-lg "
+          >
+            <IoIosLogOut />
+            <p>{t("nav.logout")}</p>
+          </button>
+        )}
+      </motion.div>
+    </>
   );
 };
 
