c5dcc77857a717d77b7c8e66655fecdabe7dcc76
foreverbog <133509015+foreverbog@users.noreply.github.com>
2024-10-04 14:34:10 +0200

    Progress
    Added charBar in the expense Dashboard as a image

--- frontend/src/components/ExpensesGrid.tsx
+++ frontend/src/components/ExpensesGrid.tsx
@@ -5,12 +5,12 @@ const ExpensesGrid = () => {
   console.log(expenses);
 
   return (
-    <div className=" relative border grid grid-cols-1  mx-auto mt-12 w-2/5  ">
+    <div className=" relative border grid grid-cols-1  mx-auto mt-12 w-full xl:w-2/5  ">
       {isLoading && <Loading text="Loading..." />}
       {expenses.length > 0 ? (
         <>
           <div
-            className="border grid grid-cols-4  w-full divide-x-2 "
+            className="border grid grid-cols-4 w-full divide-x-2 "
             style={{ gridTemplateColumns: "100px auto 150px 150px" }}
           >
             <p className="text-center">Category</p>
@@ -21,13 +21,13 @@ const ExpensesGrid = () => {
           {expenses?.map((expense) => (
             <div
               key={expense._id}
-              className="relative border grid grid-cols-4  w-full divide-x even:bg-base odd:bg-base-100 "
+              className="relative border grid grid-cols-4  w-full divide-x even:bg-base odd:bg-base-100"
               style={{ gridTemplateColumns: "100px auto 150px 150px" }}
             >
               <p className="text-center">{expense.icon}</p>
               <p>{expense.name}</p>
               <p>{expense.value}</p>
-              <p>{expense.date}</p>
+              <p className="text-ellipsis">{expense.date}</p>
             </div>
           ))}
         </>
--- frontend/src/pages/ExpenseDashboard.tsx
+++ frontend/src/pages/ExpenseDashboard.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import ExpensesHeading from "../components/ExpensesHeading";
 import ExpensesGrid from "../components/ExpensesGrid";
 import { useTranslation } from "react-i18next";
+import { MdBarChart } from "react-icons/md";
 
 const ExpenseDashboard = () => {
   const [t] = useTranslation("global");
@@ -15,7 +16,8 @@ const ExpenseDashboard = () => {
   };
 
   return (
-    <div className="bg-base   font-base text-base-text min-h-dvh flex flex-col">
+    <div className=" relative bg-base   font-base text-base-text min-h-dvh flex flex-col overflow-hidden">
+      <MdBarChart className="absolute -bottom-12 -right-12 md:-bottom-24 md:-right-12 text-[240px] md:text-[440px] opacity-20 text-base-text" />
       <h1 className="text-4xl py-6 pl-6 bg-primary text-primary-text">
         {t("menu.expense")}
       </h1>
