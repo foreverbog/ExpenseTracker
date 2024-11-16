63e04e6091bd851f328fb2bfe3c738de0062c691
foreverbog <133509015+foreverbog@users.noreply.github.com>
2024-11-16 22:11:18 +0100

    Progress
    Mockup notfound page

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,5 +1,9 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="flex justify-center items-center font-base text-base-text min-h-dvh">
+      About
+    </div>
+  );
 };
 
 export default About;
