cfd4ca5e8eadb9f93521eec83e93d0e549f28db1
foreverbog <133509015+foreverbog@users.noreply.github.com>
2024-12-02 16:38:48 +0100

    BugFix
    removed overflow-scroll was causing double sidebar

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navigation/Navbar";
 import { Outlet } from "react-router-dom";
 const MainLayout = () => {
   return (
-    <div className="bg-base flex flex-col min-h-screen overflow-scroll">
+    <div className="bg-base flex flex-col min-h-screen">
       <Navbar />
       <Outlet />
       <Footer />
